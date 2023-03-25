a432c562607721541b3143353b968c53e9426f87
Doris Morales <dmorales55@gmail.com>
2023-03-24 22:55:15 -0500

    Laravel - Posts CRUD's - Images with NPM Dropzone

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -8,14 +8,35 @@ use Illuminate\Http\Request;
 
 class ImagenController extends Controller
 {
+  private $imagen_path;
+  
+  public function __construct()
+  {
+    $this->imagen_path = public_path('storage/uploads');
+  }
+
   public function store(Request $request)
   {
-    $imagen         = $request->file('file');
+    /* $imagen         = $request->file('file');
     $nombreImagen   = Str::uuid() . "." . $imagen->extension();
-    $imagenServidor = Image::make($imagen); // image::Class esta clase nos permite crear una imagen de intervention.io
+    $imagenServidor = Image::make($imagen);
     $imagenServidor->fit(100, 100);
     $imagenPath = public_path('uploads') . '/' . $nombreImagen;
-    $imagenServidor->save($imagenPath);
+    $imagenServidor->save($imagenPath); */
+
+
+    $imagen = $request->file('file');
+    
+    if (!is_dir($this->imagen_path)) {
+      mkdir($this->imagen_path);
+    }
+    
+    $nombreImagen = Str::uuid() . "." . $imagen->extension();
+    // $nombreImagen = Str::uuid() . "." . $imagen->getClientOriginalExtension();
+    
+    Image::make($imagen)->resize(100, null, function ($constraint) {
+      $constraint->aspectRatio();
+    })->save($this->imagen_path . '/' . $nombreImagen);
 
     return response()->json(['imagen' => $nombreImagen]);
   }
